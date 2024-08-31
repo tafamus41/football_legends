4901428caee28a491039522175a8247005b203e7
asimov <seyranmustafa4@gmail.com>
2024-08-31 17:03:25 +0300

    p

--- src/components/legends/LegendCard.jsx
+++ src/components/legends/LegendCard.jsx
@@ -1,9 +1,22 @@
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 
-const LegendCard = () => {
+const LegendCard = ({legend}) => {
   return (
-    <div>LegendCard</div>
-  )
-}
+    <Col xs={12} sd={8} md={6} lg={4} xl={3} >
+      <Card>
+        <Card.Img variant="top" src={legend.img} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
-export default LegendCard
+export default LegendCard;
