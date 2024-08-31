73a5e4ab9c3817a5ba8213b20965562470eff975
asimov <seyranmustafa4@gmail.com>
2024-08-31 20:06:07 +0300

    update

--- src/components/legends/LegendsContainer.jsx
+++ src/components/legends/LegendsContainer.jsx
@@ -3,15 +3,18 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import LegendCard from "./LegendCard";
+import React,{useState} from "react"
 
 const LegendsContainer = () => {
   // console.log(data);
+  const [search,setSearch]=useState("");
+
   return (
     <>
-      <Form.Control type="search" className="w-50 mx-auto my-2" placeholder="Search legends..."/>
-      <Container>
+      <Form.Control onChange={(e)=>setSearch(e.target.value)} type="search" className="w-50 mx-auto my-2" placeholder="Search legends..."/>
+      <Container className="p-3 rounded-4 card-container my-3">
         <Row>
-          {data.map((legend) => (
+          {data.filter((legend)=>legend.name.toLowerCase().includes(search.trim().toLowerCase())).map((legend) => (
             <LegendCard key={legend.id} legend={legend} />
           ))}
         </Row>
