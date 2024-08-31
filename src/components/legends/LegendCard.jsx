ef42117d9c3c133df5f163ad4313cf5f298222c3
asimov <seyranmustafa4@gmail.com>
2024-08-31 17:15:19 +0300

    update

--- src/components/legends/LegendCard.jsx
+++ src/components/legends/LegendCard.jsx
@@ -1,18 +1,23 @@
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
-
-const LegendCard = ({legend}) => {
+const LegendCard = ({ legend }) => {
   return (
-    <Col xs={12} sd={8} md={6} lg={4} xl={3} >
+    <Col xs={12} sd={8} md={6} lg={4} xl={3}>
       <Card>
         <Card.Img variant="top" src={legend.img} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>{legend.name}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            {legend.statistics.map((item, index) => (
+              <ul>
+                <li key={index}>{item}</li>
+              </ul>
+            ))}
           </Card.Text>
+          <Card.Footer className="text-muted">
+            Career Years: {legend.official_career}
+          </Card.Footer>
         </Card.Body>
       </Card>
     </Col>
