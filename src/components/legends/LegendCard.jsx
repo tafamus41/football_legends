5580e9d08b07653977277a33d2c2a62a55d7efba
asimov <seyranmustafa4@gmail.com>
2024-08-31 17:59:41 +0300

    useState

--- src/components/legends/LegendCard.jsx
+++ src/components/legends/LegendCard.jsx
@@ -1,22 +1,29 @@
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import { useState } from "react";
 
 const LegendCard = ({ legend }) => {
+  const [image, setImage] = useState(true);
   return (
     <Col xs={12} sd={8} md={6} lg={4} xl={3}>
-      <Card>
-        <Card.Img variant="top" src={legend.img} />
-        <Card.Body>
-          <Card.Header>{legend.name}</Card.Header>
-          <ul>
-            {legend.statistics.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
-          <Card.Footer className="text-muted">
-            Career Years: {legend.official_career}
-          </Card.Footer>
-        </Card.Body>
+      <Card onClick={() => setImage(!image)} className="legend-card">
+        {image ? (
+          <Card.Img variant="top" src={legend.img} />
+        ) : (
+          <Card.Body>
+            <Card.Header>{legend.name}</Card.Header>
+            <ul className="m-auto ps-0">
+              {legend.statistics.map((item, index) => (
+                <li key={index} className="list-unstyled h5 text-start">
+                  ⚽{item}
+                </li>
+              ))}
+            </ul>
+            <Card.Footer className="text-muted">
+              Career Years: {legend.official_career}
+            </Card.Footer>
+          </Card.Body>
+        )}
       </Card>
     </Col>
   );
